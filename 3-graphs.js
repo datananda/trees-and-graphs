76ae74354b749cf036babfb1d7946f1e77301a46
Madeleine Filloux <mfilloux@gmail.com>
2020-05-09 16:59:59 -0700

    add breadth and depth first traversal for graphs

--- 3-graphs.js
+++ 3-graphs.js
@@ -29,10 +29,30 @@ class Graph {
         this.adjList[node2.value].edges = this.adjList[node2.value].edges.filter(nodeInList => nodeInList !== node1);
     }
   
-    depthFirstTraversal(startingNode, func = console.log) {
+    depthFirstTraversal(startingNode, visited = [], func = console.log) {
+        func(startingNode);
+        visited.push(startingNode);
+        this.adjList[startingNode.value].edges.forEach(relatedNode => {
+            if (!visited.includes(relatedNode)) {
+                this.depthFirstTraversal(relatedNode, visited);
+            }
+        })
     }
   
     breadthFirstTraversal(startingNode, func = console.log) {
+        let visited = [];
+        let nodesToProcess = [startingNode];
+
+        while (nodesToProcess.length) {
+            let currentNode = nodesToProcess.shift();
+            func(currentNode);
+            visited.push(currentNode);
+            this.adjList[currentNode.value].edges.forEach(relatedNode => {
+                if (!visited.includes(relatedNode)) {
+                    nodesToProcess.push(relatedNode);
+                }
+            });
+        }
     }
   }
 
@@ -44,21 +64,22 @@ class Node {
   
 //   export default Graph;
 
-//   const myGraph = new Graph;
-//   const node1 = new Node(1);
-//   myGraph.addNode(node1);
-//   const node2 = new Node(2);
-//   myGraph.addNode(node2);
-//   const node3 = new Node(3);
-//   myGraph.addNode(node3);
+  const myGraph = new Graph;
+  const node1 = new Node(1);
+  myGraph.addNode(node1);
+  const node2 = new Node(2);
+  myGraph.addNode(node2);
+  const node3 = new Node(3);
+  myGraph.addNode(node3);
 
 //   console.log(myGraph);
 
-//   myGraph.addEdge(node1, node2);
-//   myGraph.addEdge(node3, node2);
+  myGraph.addEdge(node1, node2);
+  myGraph.addEdge(node3, node2);
 
 //   console.log(myGraph);
 
 //   myGraph.removeEdge(node1, node2);
 
 //   console.log(myGraph);
+myGraph.breadthFirstTraversal(node1);
